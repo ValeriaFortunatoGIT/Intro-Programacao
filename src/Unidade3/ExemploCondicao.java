058084e49cb2d86850883c242a8e4253c8d48015
Valéria Fortunato <ValeriaFortunatoGIT>
2026-09-09 20:31:27 -0300

    unidade 3

--- src/Unidade3/ExemploCondicao.java
+++ src/Unidade3/ExemploCondicao.java
@@ -1,5 +1,13 @@
 package Unidade3;
 
 public class ExemploCondicao {
+public static void main(String[] args) {
+    String tempo = "sol";
 
+    if(tempo.equals("sol")){
+        System.out.println("Leve o oculos de sol");
+    }else{
+        System.out.println("Leve o guarda chuva");
+    }
+}
 }
